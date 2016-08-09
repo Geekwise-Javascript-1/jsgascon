3ee94920c71b5017649315dc05b354c3acae9c4a
Joseph Gascon <jsgascon@live.com>
2016-08-08 20:59:50 -0700

    day01 commit

---
+++ day01/script.js
@@ -0,0 +1,26 @@
+// alert('something');
+
+var name = 'joseph';
+var a = 5;
+var b = 6;
+var c = a + b;
+
+var e = true;
+var f = false;
+var g;
+
+var nothing = null;
+var something = undefined;
+
+var arr = ['joseph', 21, false];
+// console.log(name.length);
+
+// console.log(arr.length);
+
+var car = {
+  make: 'Jeep',
+  model: 'Wrangler',
+  year: 1997
+};
+
+console.log (car.model);
