0941465c3cbc25bafc7815e8d3f7717aead3ea02
Joseph Gascon <jsgascon@live.com>
2016-08-17 00:25:30 -0700

    day03 assignment commit

--- day03/assignment.js
+++ day03/assignment.js
@@ -4,7 +4,20 @@
 .substr()
 */
 
-var name = prompt('What is your name?');
+// var name = prompt('What is your name?');
 // name = name.charAt(0).toUpperCase() + name.slice(1);
-name = name.charAt(0).toUpperCase() + name.substr(1);
-console.log(name);
+// console.log(name);
+
+// var name = prompt('What is your name?');
+// name = name.charAt(0).toUpperCase() + name.substr(1);
+// console.log(name);
+
+var name = prompt('What is your name?');
+if(name.charAt(0) == name.charAt(0).toLowerCase() && name.charAt(0) != name.charAt(0).toUpperCase()){
+  name = name.charAt(0).toUpperCase() + name.substr(1);
+  console.log(name);
+  alert('First letter is lower case.')
+}else{
+  alert('First letter is already upper case.');
+  console.log(name);
+}
