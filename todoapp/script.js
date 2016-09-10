519afd6f6aa3148f4173e400074484709d66d7e2
Joseph Gascon <jsgascon@live.com>
2016-09-09 18:25:25 -0700

    day10 commit

--- todoapp/script.js
+++ todoapp/script.js
@@ -2,7 +2,9 @@
 var theForm1 = document.getElementsByClassName('personal-info')[0];
 var theForm2 = document.getElementsByClassName('list')[0];
 var submitBtn = document.querySelector('input[type="submit"]');
+var addBtn = document.getElementById('addfield');
 submitBtn.addEventListener('click', getInputValues);
+addfield.addEventListener('click', createField);
 
 function getInputValues(e){
   e.preventDefault();
@@ -17,4 +19,24 @@ function getInputValues(e){
     linputs.push(theForm2.elements[i].value);
   }
   console.log(linputs);
+}
+
+var incNum = 1;
+function createField(){
+  var newLabel = document.createElement('label');
+  newLabel.setAttribute("for", "generic"+incNum);
+  newLabel.innerHTML = "Generic Label";
+
+  var newInput = document.createElement('input');
+  newInput.id = "generic"+incNum;
+  newInput.name = "generic";
+  newInput.type = "text";
+  newInput.placeholder = "Generic Input Box";
+
+  addToForm(newLabel, newInput);
+  incNum++;
+}
+function addToForm(newLabel, newInput){
+  newLabel.appendChild(newInput);
+  theForm2.insertBefore(newLabel, addBtn);
 }
